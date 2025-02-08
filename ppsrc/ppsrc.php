32dfbe1f761993ea10b120e32924dfea79ebeaa5
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-02-08 22:43:46 +0500

    commit for end

--- ppsrc/ppsrc.php
+++ ppsrc/ppsrc.php
@@ -5,13 +5,20 @@
 	}
 	function fetchall(){
 		$sql=connect();
-		$result = $sql->query("SELECT * FROM `products`");
-		while($row = mysqli_fetch_assoc($result)) {
-			$data[] = $row;
+		$result=$sql->query("SELECT * FROM `products`");
+		while($row=mysqli_fetch_assoc($result)) {
+			$data[]=$row;
 		}
 		return $data;
 	}
-	$data = json_encode(fetchall());
+
+	function getById($id) {
+		$sql=connect();
+		$result=$sql->query("SELECT * FROM `products` WHERE `pr_id` = '$id'");
+		$data=mysqli_fetch_assoc($result);
+		return $data;
+	}
+	$data=json_encode(fetchall());
 	echo $data;
 
 ?>
